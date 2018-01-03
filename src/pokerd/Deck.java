41cff5c68198617117110ccb71727bc7bce02b41
EunhoKim219 <eunho.kim@stonybrook.edu>
2018-01-03 17:14:54 -0500

    first commit

--- src/pokerd/Deck.java
+++ src/pokerd/Deck.java
@@ -13,9 +13,60 @@ import java.util.Stack;
  * @author Chris
  */
 public class Deck {
-    Stack<Card> deck;
+    Stack<Card> deck = new Stack();
     public void Deck() {
-        
+        char num = '2';
+        for (int i = 0; i < 9; i++) {
+            Card c = new Card(num, 'h');
+            deck.add(c);
+            num++;
+        }
+        Card c = new Card('a', 'h');
+        deck.add(c);
+        c = new Card('j', 'h');
+        deck.add(c);
+        c = new Card('q', 'h');
+        deck.add(c);
+        c = new Card('k', 'h');
+        deck.add(c);
+        num = '2';
+        for (int i = 0; i < 9; i++) {
+            c = new Card(num, 'd');
+            deck.add(c);
+            num++;
+        }
+        num = '2';
+        c = new Card('a', 'd');
+        deck.add(c);
+        c = new Card('j', 'd');
+        deck.add(c);
+        c = new Card('q', 'd');
+        deck.add(c);
+        c = new Card('k', 'd');
+        deck.add(c);
+        for (int i = 0; i < 9; i++) {
+            c = new Card(num, 'c');
+        }
+        c = new Card('a', 'c');
+        deck.add(c);
+        c = new Card('j', 'c');
+        deck.add(c);
+        c = new Card('q', 'c');
+        deck.add(c);
+        c = new Card('k', 'c');
+        deck.add(c);
+        num = '2';
+        for (int i = 0; i < 9; i++) {
+            c = new Card(num, 's');
+        }
+        c = new Card('a', 's');
+        deck.add(c);
+        c = new Card('j', 's');
+        deck.add(c);
+        c = new Card('q', 's');
+        deck.add(c);
+        c = new Card('k', 's');
+        deck.add(c);
     }
     
     public void shuffle() {
@@ -23,6 +74,8 @@ public class Deck {
     }
     
     public void printDeck() {
-        
+        for (Card c : deck) {
+            System.out.println(c.suit + c.num);
+        }
     }
 }
