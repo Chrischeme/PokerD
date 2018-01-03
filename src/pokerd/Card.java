e50a59f9138e5d34684e702d2802e067f8aa4b2f
EunhoKim219 <eunho.kim@stonybrook.edu>
2018-01-03 17:03:28 -0500

    initial commit

---
+++ src/pokerd/Card.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokerd;
+
+/**
+ *
+ * @author Chris
+ */
+public class Card {
+    char num = '\0';
+    char suit = '\0';
+
+    Card(char num, char suit) {
+        this.num = num;
+        this.suit = suit;
+    }
+}
---
+++ src/pokerd/Deck.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokerd;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ *
+ * @author Chris
+ */
+public class Deck {
+    Stack<Card> deck;
+    public void Deck() {
+        
+    }
+    
+    public void shuffle() {
+        
+    }
+    
+    public void printDeck() {
+        
+    }
+}
